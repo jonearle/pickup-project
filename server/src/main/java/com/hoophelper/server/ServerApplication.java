126a3b028aa91c1ecd428193195ffc54b20bce5d
Jon Earle <jonearle05@gmail.com>
2025-11-10 10:12:15 -0400

    Quick change to serverapplication

--- server/src/main/java/com/hoophelper/server/ServerApplication.java
+++ server/src/main/java/com/hoophelper/server/ServerApplication.java
@@ -2,25 +2,12 @@ package com.hoophelper.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "com.hoophelper.server")
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOriginPatterns("http://localhost:5173").allowCredentials(true);
-			}
-		};
 	}
 
 }
